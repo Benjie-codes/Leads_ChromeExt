b5304058391a8b3ebb9f91f8eeb09649031b0bb3
Benjamin Samuel <me.benjhamine@gmail.com>
2023-11-24 01:05:44 +0000

    update

--- excercise.js
+++ excercise.js
@@ -3,3 +3,9 @@ let clickEvent = document.getElementById("box")
 clickEvent.addEventListener("click", function(){
     clickEvent.textContent = "I want to open the box!"
 })
+
+let buyBtn = document.getElementById("container")
+buyBtn.innerHTML = "<button>" + "Buy" + "</button>"
+buyBtn.addEventListener("click", function() {
+    buyBtn.innerHTML += "<p>" + "Thank you for buying!" + "</p>"
+})
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-// function saveLead() {
-//     console.log("Button clicked!")
-// }
-
-let inputBtn = document.getElementById("input-btn")
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEL = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function(){
-    console.log("Button Clicked!")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
+
+for (i = 0; i < myLeads.length; i++){
+    ulEL.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
+
